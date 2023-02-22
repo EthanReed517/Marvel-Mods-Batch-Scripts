a29985b5f4755f40f9ed64997f9869de46f71f23
EthanReed517 <ethanreed517@gmail.com>
2023-02-22 17:17:37 -0600

    Sound Installer - Syntax changes based on feedback

--- Easy Installers/Sound Installer/soundInstaller.bat
+++ Easy Installers/Sound Installer/soundInstaller.bat
@@ -2,12 +2,12 @@
 
 REM define initial variables
 REM File path where the sounds should be installed (if blank, will ask)
-set filePath=""
+set "%filePath%="
 REM Select a language (eng = English, blank = ask)
 set language="eng"
 
 REM see if the file path is defined
-if %filePath%=="" (
+if "%filePath%"=="" (
 	REM get the file path
 	set /p "filePath=Enter your game directory or an MO2 mod folder: "
 )
@@ -26,10 +26,10 @@ for %%a in (%*) do (
 	REM get the file name
 	set fileName=%%~na
 	REM define the destination path for the sound
-	set soundPath=!filepath!\sounds\!language!\!fileName:~0,1!\!fileName:~1,1!
+	set "soundPath=!filepath!\sounds\!language!\!fileName:~0,1!\!fileName:~1,1!"
 	REM create the destination if it doesn't exist
-	if not exist !soundPath! md !soundPath!
+	if not exist "!soundPath!" md "!soundPath!"
 	REM copy the sound
-	copy %%a !soundPath!
+	copy "%%~a" "!soundPath!"
 )
 pause
