7a856faa11cec6c53518ae71d41bdbcd942007be
ak2yny <92672223+ak2yny@users.noreply.github.com>
2023-05-27 17:12:35 +0200

    Added Alchemy 5 Setup Script
    Latest changes:
    - Fixed a bug with the latest Win 11 versions where an exit error would display.
    - Added the option to uninstall on identical installation again.

---
+++ MUA Registry+ Scripts/Setup Script for Alchemy 5 Portable/ReadMe.md
@@ -0,0 +1,12 @@
+Instructions:
+Run Setup.bat with Administrator rights.
+  - If the command window doesn't open (flashes only), there is a problem with the current path.
+    In this case, run Setup.bat from a NEW elevated command prompt (https://www.youtube.com/watch?v=tK6jJFsLnqY), and:
+    Type the path and name of the Setup BAT, including double-quotes, eg. "C:\Alchemy\Setup.bat", and press enter.
+  - Associate IGB files with Finalizer (view and modify content as text), Insight viewer (view IGB visually), or not, as you want.
+This allows Alchemy batches/tools to run correctly, and associates IGB files with the Alchemy 5 Finalizer.
+
+Hints:
+If you move Alchemy Portable to a different folder, you can run Setup.bat again to update the information.
+If you want to remove Alchemy Portable, you can run Setup.bat - if it is registered, you can choose to uninstall it.
+A reboot or login may be required for the dll libraries to register correctly (path variable).
---
+++ MUA Registry+ Scripts/Setup Script for Alchemy 5 Portable/Setup.bat
@@ -0,0 +1,124 @@
+@echo off
+
+echo "%~dp0" | find ";" >nul && goto ErrorPath
+
+set envkey="HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment"
+if defined IG_ROOT set "OldDLL=%IG_ROOT:"=%\DLL"
+if defined OldDLL set "OldDLL=%OldDLL:\\=\%"
+set "NewDLL=%~dp0DLL"
+
+if "%NewDLL%" == "%OldDLL%" (
+ choice /m "An identical installation was found. Do you want to unregister Alchemy from this system"
+ if ERRORLEVEL 2 Exit 0
+ goto Uninstall
+)
+if defined IG_ROOT (
+ choice /m "Alchemy found. Do you want to unregister Alchemy from this system (press N to update the setup instead)"
+ if not ERRORLEVEL 2 goto Uninstall
+)
+
+:Install
+setx IG_ROOT /m "%~dp0
+if ERRORLEVEL 1 goto Error
+echo "%PATH:"=%" | find /i "%OldDLL%" >nul && call :RemPathVar
+call :GetPathVar
+echo "%OldPath:"=%" | find /i "%NewDLL%" >nul || call :AddPathVar
+choice /c FIN /m "Associate .IGB files with Finalizer (F), Insight Viewer (I) or don't associate/change (N)"
+if ERRORLEVEL 3 Exit 0
+if ERRORLEVEL 2 ( set "igApp=insight\DX9\insight" ) else ( set "igApp=Finalizer\sgFinalizer" )
+FTYPE igb_auto_file="%~dp0ArtistPack\%igApp%.exe" "%%1"
+ASSOC .IGB=igb_auto_file
+Exit 0
+
+:Uninstall
+setx IG_ROOT "" /m
+if ERRORLEVEL 1 goto Error
+echo "%PATH:"=%" | find /i "%OldDLL%" >nul && call :RemPathVar
+REG delete %envkey% /f /v IG_ROOT
+FTYPE igb_auto_file=
+ASSOC .IGB=
+pause
+Exit 0
+
+REM [User] is something like "S-1-5-21-xxxxxxxxxx-xxxxxxxxx-xxxxxxxxxx-1001"
+REM [HKCU] is either HKEY_USERS\[User] or HKEY_CURRENT_USER   (User key)
+REM [HKCR] is [HKCU]\SOFTWARE\Classes, [HKCU]_Classes, or HKEY_CLASSES_ROOT
+REM [CS] is HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001 (etc.) or HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet
+REM [CV] is [HKCU]\SOFTWARE\Microsoft\Windows\CurrentVersion
+REM Partial user key: [CS]\Services\bam\State\UserSettings\[User]
+
+REM File assiociation is not properly removed. To do so, you would have to edit registry directly, which is not so secure in this case.
+REM [HKCR]\igb_auto_file                       should be removed with FTYPE
+REM (not found in latest test: [HKCR]\.igb)    should be removed with ASSOC
+REM (not found in latest test: [HKCR]\SgFinalizer.Document)
+REM    (plus: [HKCR]\Applications\insight.exe) > open with
+REM    (plus: [HKCR]\Applications\sgFinalizer.exe) > open with
+REM [CV]\ApplicationAssociationToasts
+REM       igb_auto_file_.igb
+REM       (not found in latest test: Applications\insight.exe_.igb and/or Applications\sgFinalizer.exe_.igb)
+REM [CV]\Explorer\FileExts\.igb
+
+REM There are multiple other registry entries for Alchemy and Finalizer after registering file association and using the app.
+REM [HKCU]\SOFTWARE\Alchemy Finalizer
+REM [HKCR]\Local Settings\Software\Microsoft\Windows\Shell\MuiCache
+REM       all Alchemy apps are listed here
+REM [CV]\Explorer\FeatureUsage\AppSwitched
+REM       installer and viewer/finalizer are listed here
+REM [CV]\Explorer\FeatureUsage\ShowJumpView
+REM       installer and viewer/finalizer are listed here
+REM [HKCU]\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\
+REM       Compatibility Assistant\Store & Layers: viewer/finalizer comp. settings
+REM Code example: REG delete "HKEY_CURRENT_USER\SOFTWARE\Alchemy Finalizer" /f
+
+:AddPathVar
+set "NewPath=%OldPath:"=%;%NewDLL%;"
+goto RegPathVar
+REM Powershell takes way too long, but no prompt
+PowerShell "[System.Environment]::SetEnvironmentVariable('PATH', [System.Environment]::GetEnvironmentVariable('PATH', 'machine') + ';%NewDLL%;', 'machine')"
+
+:RemPathVar
+set "Equal=%OldDLL%#" & call :fixEqual
+set "r=;%ne:~1,-1%"
+call :GetPathVar
+set "Equal=%OldPath:"=%#" & set "ne=" & call :fixEqual
+set "OldPath=%ne:~1,-1%"
+call set "NewPath=%%OldPath:%r%=%%"
+set "NewPath=%NewPath:?==%"
+call :RegPathVar
+echo "%NewPath%" | findstr /irc:"Alchemy.*DLL" >nul && call :Warnng
+Exit /b
+:fixEqual
+for /F "tokens=1* delims==" %%a in ("%Equal%") do set "Equal=%%b" & set "ne=%ne%?%%a"
+if defined Equal goto fixEqual
+Exit /b
+
+:RegPathVar
+REG add %envkey% /f /v Path /t REG_SZ /d "%NewPath:;;=;%"
+if ERRORLEVEL 1 goto Error
+Exit /b
+
+:GetPathVar
+for /f "skip=2 tokens=1,2*" %%n in ('REG query %envkey% /v "Path" 2^>nul') do if /i "%%n" == "Path" set "OldPath=%%p"
+Exit /b
+
+:Warnng
+echo.
+echo  An old Alchemy path still exists in the Path variable.
+echo  Please remove or update it manually:
+echo   Control Panel ^> System ^> Advanced system settings (near bottom) ^> Environment Variables.
+echo   Select the "Path" variable (System and/or User) and click on "Edit".
+pause
+Exit /b
+
+:ErrorPath
+echo ERROR: A ^"^;^" was found in the path.
+echo Please rename the files and^/or folders,
+echo or move Alchemy to a different location, and try again.
+echo Unfotunately, the variables to register are malfunctioning with a ^"^;^".
+pause
+Exit
+
+:Error
+endlocal
+echo Please run the Setup with administrator rights.
+pause
